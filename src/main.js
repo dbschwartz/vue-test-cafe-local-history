6da22a62790cd477b5fe0077bc3021b7cc5c770b
David Schwartz <dbschwartz2@gmail.com>
2019-04-27 10:56:02 -0600

    master trying to make set local storage programmatic

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-localStorage.setItem('link', JSON.stringify([{
-  testObj: {abcdef: '12345', ghijkl: '123123'},
-  link: "https://router.vuejs.org"
-}]));
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
